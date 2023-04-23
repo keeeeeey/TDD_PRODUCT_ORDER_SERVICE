7060fc800c37d1c050a75a2d79721bb697703dcc
keeeeeey <sseioul@naver.com>
2023-04-24 06:36:51 +0900

    Fix: pojo text -> SpringBootTest

---
+++ src/main/java/com/example/productorderservice/order/CreateOrderRequest.java
@@ -0,0 +1,20 @@
+package com.example.productorderservice.order;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.util.Assert;
+
+@Getter
+@NoArgsConstructor
+class CreateOrderRequest {
+
+    private Long productId;
+    private int quantity;
+
+    public CreateOrderRequest(Long productId, int quantity) {
+        Assert.notNull(productId, "상품 ID는 필수입니다.");
+        Assert.isTrue(quantity > 0, "수량은 0보다 커야 합니다.");
+        this.productId = productId;
+        this.quantity = quantity;
+    }
+}
---
+++ src/main/java/com/example/productorderservice/order/OrderService.java
@@ -0,0 +1,21 @@
+package com.example.productorderservice.order;
+
+import com.example.productorderservice.product.Product;
+import org.springframework.stereotype.Component;
+
+@Component
+class OrderService {
+    private final OrderPort orderPort;
+
+    OrderService(OrderPort orderPort) {
+        this.orderPort = orderPort;
+    }
+
+    public void createOrder(CreateOrderRequest request) {
+        Product product = orderPort.getProductById(request.getProductId());
+
+        final Order order = new Order(product, request.getQuantity());
+
+        orderPort.save(order);
+    }
+}
